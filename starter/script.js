c6d36acb2b2be1782dffdd268cee951515122660
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-13 21:48:03 +0100

    selecting elements with ids and stating starting conditions

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+//selecting elements
+const score0El = document.querySelector('#score--0');
+
+const score1El = document.getElementById('score--1'); //another way to select elements that work by ids
+
+const diceEl = document.querySelector('.dice');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
