940bb4d594446411d3bd7bbed7bb51baf7c57c9f
Ehicodes <osaigbovoehinomen@gmail.com>
2025-04-14 22:09:00 +0100

    switching the active players and introduction to .toggle method

--- starter/script.js
+++ starter/script.js
@@ -11,12 +11,18 @@ const btnHold = document.querySelector('.btn--gold');
 
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
-let currentScore = 0; //it cannot be inside theh function because each time we click the button, it will be set to 0
 
+const scores = [0, 0];
+let currentScore = 0; //it cannot be inside theh function because each time we click the button, it will be set to 0
+let activePlayer = 0;
 //rolling dice  functionality
 btnRoll.addEventListener('click', function () {
   // 1. Generate a random dice roll
@@ -24,14 +30,22 @@ btnRoll.addEventListener('click', function () {
   console.log(dice);
   // 2. display the dice
   diceEl.classList.remove('hidden');
+
   //manipulating our source attribute from our JavaScript because we want to display the dice image according to the dice number generated
   diceEl.src = `dice-${dice}.png`;
   // 3. check for a rolled 1..
   if (dice !== 1) {
     //if false, add dice  to current score
-    currentScore = currentScore + dice; //or  currentScore+= dice
-    current0El.textContent = 'currentScore';
+    currentScore = currentScore + dice;
+    // current0El.textContent = currentScore; //CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore; //a more dynamic way to select the element
   } else {
     //if true, switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active'); //toggle will add the class if it is not there or remove the class if it is there
+    player1El.classList.toggle('player--active');
   }
 });
